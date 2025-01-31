85c98fa2034b6bcb6fd5e3c098b7d5c9d6e67f4d
zabooh <martin.ruppert@microchip.com>
2025-01-31 15:41:40 +0100

    FFT Experiment with artificial signal

--- firmware/src/main.c
+++ firmware/src/main.c
@@ -54,6 +54,7 @@
 #include <string.h>
 #include "definitions.h"                // SYS function prototypes
 #include "goertzel.h"
+#include "fft_32BitInplace.h"
 
 
 /*  STREAM_DATA allows to send 2*512 Samples and Goertzel Results by UART
@@ -71,7 +72,7 @@
  * to check and compare the signal processing with an "perfect" signal
  * Can be used for Realtime
  */
-//#define USE_ARTIFICIAL_SIGNAL
+#define USE_ARTIFICIAL_SIGNAL
 
 /* SIGNAL_SCALE_THRESHOLD set the threshold for the Frequency detection
  */
@@ -89,7 +90,7 @@ int sine_wave_table_index = 0; // Current position in the wave table
 GOERTZEL_f_COEFF goertzel_float_coeff; // Control date for Goertzel Float 
 GOERTZEL_i_COEFF goertzel_integer_coeff; // Control date for Goertzel Fixpoint
 
-float input_voltage; 
+float input_voltage;
 float test_voltage = 0.0;
 
 volatile int32_t dma_on = 1;
@@ -108,7 +109,6 @@ int32_t output_data_1[BLOCK_SIZE];
 
 #define WAVE_TABLE_AMPLITUDE_SHIFT 1
 
-
 void DmacCh0Cb(DMAC_TRANSFER_EVENT returned_evnt, uintptr_t MyDmacContext) {
     if (DMAC_TRANSFER_EVENT_COMPLETE == returned_evnt) {
         dma_ch0Done = true;
@@ -116,13 +116,13 @@ void DmacCh0Cb(DMAC_TRANSFER_EVENT returned_evnt, uintptr_t MyDmacContext) {
             buffer_current_n = 1;
             GPIO_PB16_Set();
             if (dma_on == 1) {
-                DMAC_ChannelTransfer(DMAC_CHANNEL_0, (const void *) &ADC_REGS->ADC_RESULT, (const void *) &adc_result_array[1][0], sizeof (adc_result_array)/2);
+                DMAC_ChannelTransfer(DMAC_CHANNEL_0, (const void *) &ADC_REGS->ADC_RESULT, (const void *) &adc_result_array[1][0], sizeof (adc_result_array) / 2);
             }
         } else {
             buffer_current_n = 0;
             GPIO_PB16_Clear();
             if (dma_on == 1) {
-                DMAC_ChannelTransfer(DMAC_CHANNEL_0, (const void *) &ADC_REGS->ADC_RESULT, (const void *) &adc_result_array[0][0], sizeof (adc_result_array)/2);
+                DMAC_ChannelTransfer(DMAC_CHANNEL_0, (const void *) &ADC_REGS->ADC_RESULT, (const void *) &adc_result_array[0][0], sizeof (adc_result_array) / 2);
             }
         }
     } else if (DMAC_TRANSFER_EVENT_ERROR == returned_evnt) {
@@ -139,21 +139,21 @@ uint16_t adc_value;
 
 SYSTICK_TIMEOUT m_timeout;
 
-int __attribute__((optimize("-O1"))) main(void) {
+int __attribute__((optimize("-O0"))) main(void) {
     /* Initialize all modules */
     SYS_Initialize(NULL);
-
+    
     ADC_Enable();
 
     RTC_Timer32Start();
     RTC_Timer32CompareSet(RTC_COMPARE_VAL);
 
     SYSTICK_TimerStart();
-
+    
     DMAC_ChannelCallbackRegister(DMAC_CHANNEL_0, DmacCh0Cb, (uintptr_t) & myAppObj);
 
     buffer_current_n = 0;
-    DMAC_ChannelTransfer(DMAC_CHANNEL_0, (const void *) &ADC_REGS->ADC_RESULT, (const void *) &adc_result_array[0][0], sizeof (adc_result_array)/2);
+    DMAC_ChannelTransfer(DMAC_CHANNEL_0, (const void *) &ADC_REGS->ADC_RESULT, (const void *) &adc_result_array[0][0], sizeof (adc_result_array) / 2);
 
     uint32_t uTC3_TimerFrequency = TC3_TimerFrequencyGet();
     float fTC3_TimerFrequency = (float) uTC3_TimerFrequency;
@@ -162,6 +162,8 @@ int __attribute__((optimize("-O1"))) main(void) {
     float fSampleRate = 1.0 / ((fTC3_Timer16bitPeriod + 1.0)* (1.0 / fTC3_TimerFrequency));
     uint32_t uSampleRate = (uint32_t) fSampleRate;
 
+//    Test_FFT(uSampleRate);
+    
 
 #ifdef USE_FLOAT_GOERTZEL
     Goertzel_f_Init(SAMPLERATE, FREQUENCY, &goertzel_float_coeff);
@@ -170,12 +172,12 @@ int __attribute__((optimize("-O1"))) main(void) {
 #endif    
 
     FLT_vIIR_Init();
-    
+
     printf("\n\r---------------------------------------------------------");
     printf("\n\r Description         ADC DMA %d Pulse Detection              ", FREQUENCY);
     printf("\n\r Build Time:         "__DATE__" "__TIME__);
     printf("\n\r BLOCK_SIZE:         %d", BLOCK_SIZE);
-    printf("\n\r BLOCK TIME:         %f",  ((1.0/SAMPLERATE) * BLOCK_SIZE) ); 
+    printf("\n\r BLOCK TIME:         %f", ((1.0 / SAMPLERATE) * BLOCK_SIZE));
     printf("\n\r FREQUENCY:          %d", FREQUENCY);
     printf("\n\r GOERTZEL_THRESHOLD: %d", GOERTZEL_THRESHOLD);
     printf("\n\r DAMPING_FACTOR:     %f", DAMPING_FACTOR);
@@ -185,6 +187,7 @@ int __attribute__((optimize("-O1"))) main(void) {
 
     TC3_TimerStart();
     SYSTICK_StartTimeOut(&m_timeout, 1000);
+
     
     while (true) {
 
@@ -194,15 +197,15 @@ int __attribute__((optimize("-O1"))) main(void) {
             beat_sum += goertzel_integer_coeff.counter;
             beat_average = beat_sum / seconds_counter;
 #ifndef STREAM_DATA               
-            printf("Second %d \tBeat %d \tAverage %d \tAmplitude %d threshold %d\n", 
-                    (int) seconds_counter, 
-                    (int) goertzel_integer_coeff.counter, 
-                    (int) beat_average, 
-                    (int) goertzel_integer_coeff.max_amplitude, 
+            printf("Second %d \tBeat %d \tAverage %d \tAmplitude %d threshold %d\n",
+                    (int) seconds_counter,
+                    (int) goertzel_integer_coeff.counter,
+                    (int) beat_average,
+                    (int) goertzel_integer_coeff.max_amplitude,
                     (int) goertzel_integer_coeff.goertzel_threshold);
 #endif
             goertzel_float_coeff.goertzel_threshold =
-            goertzel_integer_coeff.goertzel_threshold = 
+                    goertzel_integer_coeff.goertzel_threshold =
                     (goertzel_integer_coeff.max_amplitude * ((int32_t) (SIGNAL_SCALE_THRESHOLD * FIXED_POINT_SCALE))) >> FIXED_POINT_BITS;
 
             goertzel_integer_coeff.max_amplitude = 0;
@@ -217,14 +220,14 @@ int __attribute__((optimize("-O1"))) main(void) {
             if (buffer_current_n == 0) {
 #ifndef STREAM_DATA                    
                 GPIO_PB17_Set();
-                
+
 #ifdef USE_ARTIFICIAL_SIGNAL                
                 for (int ix = 0; ix < BLOCK_SIZE; ix++) {
-                    adc_result_array_1[ix] = (sine_wave[sine_wave_table_index++] << WAVE_TABLE_AMPLITUDE_SHIFT)+512;
+                    adc_result_array_1[ix] = (sine_wave[sine_wave_table_index++] << WAVE_TABLE_AMPLITUDE_SHIFT) + 512;
                     if (sine_wave_table_index >= NUM_OF_SAMPLES) sine_wave_table_index = 0;
                 }
 #endif                
-                Goertzel_i_Filter(adc_result_array_1, output_data_1, &goertzel_integer_coeff);
+                Goertzel_i_Filter(&adc_result_array[1][0], output_data_1, &goertzel_integer_coeff);
                 GPIO_PB17_Clear();
 #endif                
             } else {
@@ -232,11 +235,11 @@ int __attribute__((optimize("-O1"))) main(void) {
                 GPIO_PB17_Set();
 #ifdef USE_ARTIFICIAL_SIGNAL                
                 for (int ix = 0; ix < BLOCK_SIZE; ix++) {
-                    adc_result_array_0[ix] = (sine_wave[sine_wave_table_index++] << WAVE_TABLE_AMPLITUDE_SHIFT)+512;
+                    adc_result_array_0[ix] = (sine_wave[sine_wave_table_index++] << WAVE_TABLE_AMPLITUDE_SHIFT) + 512;
                     if (sine_wave_table_index >= NUM_OF_SAMPLES) sine_wave_table_index = 0;
                 }
 #endif
-                Goertzel_i_Filter(adc_result_array_0, output_data_0, &goertzel_integer_coeff);
+                Goertzel_i_Filter(&adc_result_array[0][0], output_data_0, &goertzel_integer_coeff);
                 GPIO_PB17_Clear();
 #endif                
 
@@ -248,11 +251,11 @@ int __attribute__((optimize("-O1"))) main(void) {
 
 #ifdef USE_ARTIFICIAL_SIGNAL
                 for (int ix = 0; ix < BLOCK_SIZE; ix++) {
-                    adc_result_array_0[ix] = (sine_wave[sine_wave_table_index++] << WAVE_TABLE_AMPLITUDE_SHIFT)+512;
+                    adc_result_array[0][ix] = (sine_wave[sine_wave_table_index++] << WAVE_TABLE_AMPLITUDE_SHIFT) + 512;
                     if (sine_wave_table_index >= NUM_OF_SAMPLES) sine_wave_table_index = 0;
                 }
                 for (int ix = 0; ix < BLOCK_SIZE; ix++) {
-                    adc_result_array_1[ix] = (sine_wave[sine_wave_table_index++] << WAVE_TABLE_AMPLITUDE_SHIFT)+512;                    
+                    adc_result_array[1][ix] = (sine_wave[sine_wave_table_index++] << WAVE_TABLE_AMPLITUDE_SHIFT) + 512;
                     if (sine_wave_table_index >= NUM_OF_SAMPLES) sine_wave_table_index = 0;
                 }
 #endif
@@ -260,14 +263,25 @@ int __attribute__((optimize("-O1"))) main(void) {
 #ifdef USE_FLOAT_GOERTZEL
                 Goertzel_f_Filter(adc_result_array_0, output_data_0, &goertzel_float_coeff);
 #else
-                Goertzel_i_Filter(&adc_result_array[0][0], output_data_0, &goertzel_integer_coeff);
+           //     Goertzel_i_Filter(&adc_result_array[0][0], output_data_0, &goertzel_integer_coeff);
 #endif                           
                 GPIO_PB17_Clear();
                 GPIO_PB17_Set();
 #ifdef USE_FLOAT_GOERTZEL
                 Goertzel_f_Filter(adc_result_array_1, output_data_1, &goertzel_float_coeff);
 #else
-                Goertzel_i_Filter(&adc_result_array[1][0], output_data_1, &goertzel_integer_coeff);
+           //     Goertzel_i_Filter(&adc_result_array[1][0], output_data_1, &goertzel_integer_coeff);
+                
+                GPIO_PB17_Set();
+                int32_t *ptr;
+               
+                // measured 224 milli seconds with float
+                // measured  60 milli seconds with fixpoint
+               
+                ptr =  FFT_32BitInplace((uint16_t *)adc_result_array, 1024);
+                GPIO_PB17_Clear();
+                for(int ix=0;ix<512;ix++)output_data_0[ix] = (*ptr++)*20;
+                //for(int ix=0;ix<512;ix++)output_data_0[ix] = iFLT_IIR1_Lowpass(0,(*ptr++)*20);
 #endif  
                 GPIO_PB17_Clear();
 
@@ -277,7 +291,7 @@ int __attribute__((optimize("-O1"))) main(void) {
                     for (sample = 0; sample < BLOCK_SIZE; sample++) {
                         adc_value = adc_result_array[0][sample];
                         input_voltage = (float) adc_value * ADC_VREF / 4095U;
-                        printf("ADC Count [%d] = %04d, ADC Input Voltage = %f V Goertzel = %d\n", sample,              (int) adc_value, input_voltage, (int) output_data_0[sample]);
+                        printf("ADC Count [%d] = %04d, ADC Input Voltage = %f V Goertzel = %d\n", sample, (int) adc_value, input_voltage, (int) output_data_0[sample]);
                         SYSTICK_DelayMs(7);
                     }
                     ix = 0;
@@ -287,10 +301,10 @@ int __attribute__((optimize("-O1"))) main(void) {
                         printf("ADC Count [%d] = %04d, ADC Input Voltage = %f V Goertzel = %d\n", sample + BLOCK_SIZE, (int) adc_value, input_voltage, (int) output_data_1[sample]);
                         SYSTICK_DelayMs(7);
                     }
-                }                
+                }
                 buffer_current_n = 0;
                 dma_on = 1;
-                DMAC_ChannelTransfer(DMAC_CHANNEL_0, (const void *) &ADC_REGS->ADC_RESULT, (const void *) &adc_result_array[0][0], sizeof (adc_result_array)/2);
+                DMAC_ChannelTransfer(DMAC_CHANNEL_0, (const void *) &ADC_REGS->ADC_RESULT, (const void *) &adc_result_array[0][0], sizeof (adc_result_array) / 2);
 
 #endif          
 
